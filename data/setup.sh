8bedfa7d01fdbb15623df5eaa5f3a6edae1faecf
Felipe Sanchez <lfelipe1501@gmail.com>
2021-10-14 17:24:49 -0500

    Deploy DB

---
+++ data/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Script to create MySQL db + user
+#
+# @author   Luis Felipe <lfelipe1501@gmail.com>
+# @website  https://www.lfsystems.com.co
+# @version  1.0
+
+SQL1="CREATE DATABASE IF NOT EXISTS ${DBUP};GRANT USAGE ON *.* TO '${USERDB}'@'%' IDENTIFIED BY '${UPWDB}';"
+SQL2="GRANT ALL privileges ON ${DBUP}.* TO '${USERDB}'@'%' WITH GRANT OPTION;"
+SQL3="FLUSH PRIVILEGES;"
+SQL4="USE ${DBUP}; CREATE TABLE test_content ( id int(11) NOT NULL, mensaje text NOT NULL, nulo int(11) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1; INSERT INTO test_content (id, mensaje, nulo) VALUES (1, 'All systems and containers working!', NULL); ALTER TABLE test_content ADD PRIMARY KEY (id); COMMIT;"
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "${SQL1}${SQL2}${SQL3}${SQL4}"
